d97df73bcc04b57dce23ed45c4714398b0287283
Jack Clackett <jack@noquarter.co>
2021-06-15 12:38:31 +0100

    fix: hooks config

--- packages/api/src/lib/hooks.ts
+++ packages/api/src/lib/hooks.ts
@@ -2,6 +2,6 @@ import * as glob from "glob"
 import { HOOK_PATHS } from "./config"
 
 export function loadPrismaHooks() {
-  const filePaths = glob.sync(__dirname + "../" + HOOK_PATHS)
+  const filePaths = glob.sync(__dirname + "../../" + HOOK_PATHS)
   filePaths.forEach(require)
 }
