72433ca1c43dae966246a46e2f0dae2882578f02
Jack Clackett <jack@noquarter.co>
2021-07-09 13:21:29 +0100

    fix: add meta tags

--- packages/admin/src/pages/_document.tsx
+++ packages/admin/src/pages/_document.tsx
@@ -9,11 +9,17 @@ export default class Document extends NextDocument {
   }
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <meta name="theme-color" key="theme-color" content="#fff" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-
+          <meta name="description" content="Welcome to the Fullstack boilerplate!" key="description" />
+          <meta property="og:title" content="Fullstack boilerplate" key="title" />
+          <meta
+            property="og:description"
+            content="Welcome to the Fullstack boilerplate!"
+            key="og:description"
+          />
           {/*  eslint-disable-next-line @next/next/no-page-custom-font */}
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
