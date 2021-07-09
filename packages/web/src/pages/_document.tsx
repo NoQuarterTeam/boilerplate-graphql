ff29891408989c729823522a3e56b549e868ac74
Jack Clackett <jack@noquarter.co>
2021-07-09 13:27:10 +0100

    fix: add seo tags to web

--- packages/web/src/pages/_document.tsx
+++ packages/web/src/pages/_document.tsx
@@ -12,6 +12,14 @@ export default class Document extends NextDocument {
       <Html>
         <Head>
           <meta name="theme-color" key="theme-color" content="#000000" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <meta name="description" content="Welcome to the Fullstack boilerplate!" key="description" />
+          <meta property="og:title" content="Fullstack boilerplate" key="title" />
+          <meta
+            property="og:description"
+            content="Welcome to the Fullstack boilerplate!"
+            key="og:description"
+          />
         </Head>
         <body>
           <ColorModeScript initialColorMode="light" />
