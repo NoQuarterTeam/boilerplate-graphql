3b42e4675dc0282b86fab7c6c7e1fc6d26279b92
Jack Clackett <jack@noquarter.co>
2021-08-03 17:57:20 +0100

    fix: revert api url

--- packages/web/src/lib/config.ts
+++ packages/web/src/lib/config.ts
@@ -22,7 +22,11 @@ export const IS_DEV = !IS_PRODUCTION
 export const REDIRECT_PATH = "redirect"
 
 export const SENTRY_DSN = "https://5d0371a223bb4509902f8940cb957daf@o204549.ingest.sentry.io/5741383"
-export const API_URL = IS_PRODUCTION ? "https://boilerplate.graphcdn.app" : "http://localhost:5000/graphql"
+
+// export const API_URL = IS_PRODUCTION ? "https://boilerplate.graphcdn.app" : "http://localhost:5000/graphql"
+export const API_URL = IS_PRODUCTION
+  ? "https://nq-boilerplate.herokuapp.com/graphql"
+  : "http://localhost:5000/graphql"
 
 export const WEB_URL = IS_PRODUCTION ? "boilerplate.noquarter.co" : "localhost:3000"
 
