1d7bc63b604994f8bd53628fe7f448492df51a90
Jack Clackett <jack@noquarter.co>
2021-09-29 19:10:05 +0200

    fix: disable sentry in dev

--- packages/web/sentry.server.config.js
+++ packages/web/sentry.server.config.js
@@ -4,5 +4,6 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN || "https://5d0371a223bb4509902f8940cb957daf@o204549.ingest.sentry.io/5741383",
-  tracesSampleRate: 1.0,
+  tracesSampleRate: 2.0,
+  enabled: NODE_ENV === "production",
 })
