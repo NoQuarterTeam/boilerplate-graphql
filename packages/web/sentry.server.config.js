716a3a2fb5a06965b443b166d3e074c38e55108f
Jack Clackett <jack@noquarter.co>
2021-09-29 19:27:44 +0200

    fix: sentry next server

--- packages/web/sentry.server.config.js
+++ packages/web/sentry.server.config.js
@@ -1,6 +1,7 @@
 import * as Sentry from "@sentry/nextjs"
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+const NODE_ENV = process.env.NODE_ENV || "development"
 
 Sentry.init({
   dsn: SENTRY_DSN || "https://5d0371a223bb4509902f8940cb957daf@o204549.ingest.sentry.io/5741383",
