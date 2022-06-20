48e2bccf6739911026590bdef9a4e562ce990bc6
Jack Clackett <jack@noquarter.co>
2022-06-20 12:59:45 +0200

    refactor: update s3 setup to use local env file

--- packages/api/src/lib/s3.ts
+++ packages/api/src/lib/s3.ts
@@ -1,10 +1,10 @@
 import { S3 } from "aws-sdk/clients/all"
 
-// S3
-const S3_CONFIG = {
-  signatureVersion: "v4",
-  region: "eu-central-1",
-}
+import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from "./config"
 
 // AWS
-export const s3 = new S3(S3_CONFIG)
+export const s3 = new S3({
+  signatureVersion: "v4",
+  region: "eu-central-1",
+  credentials: { accessKeyId: AWS_ACCESS_KEY_ID, secretAccessKey: AWS_SECRET_ACCESS_KEY },
+})
