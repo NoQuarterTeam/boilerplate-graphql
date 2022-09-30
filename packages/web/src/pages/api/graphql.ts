b09281f952b8800a76168a2abd08f7431f4e1e96
Jack Clackett <jack@noquarter.co>
2022-09-30 17:23:04 +0200

    fix: more logs

--- packages/web/src/pages/api/graphql.ts
+++ packages/web/src/pages/api/graphql.ts
@@ -5,6 +5,8 @@ import { ACCESS_TOKEN, API_URL } from "lib/config"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const token = req.cookies[ACCESS_TOKEN]
+  console.log({ API_URL })
+
   return httpProxyMiddleware(req, res, {
     target: API_URL,
     headers: { authorization: token ? `Bearer ${token}` : "" },
